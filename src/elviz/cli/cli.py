869bbe914af3984a5e396158cec71a470caf7167
PBK-DEV <paul.bruno@kochta.eu>
2026-08-17 21:12:35 +0200

    add test_project

--- src/elviz/cli/cli.py
+++ src/elviz/cli/cli.py
@@ -1,16 +1,14 @@
 """Console script for elviz."""
 
 from enum import Enum
-
-from rich.console import Console
-
 from pathlib import Path
 
 import typer
-from .project import app as project
+from rich.console import Console
+
 from .circuit import app as circuit
 from .pages import app as pages
-
+from .project import app as project
 
 app = typer.Typer()
 app.add_typer(typer_instance=project, name="project")
